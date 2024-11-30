36c18e893597969a57af3a1c82cd656a72ed1cb4
fabiomartinsbrrj <fabiomartinsbr@gmail.com>
2024-11-30 16:34:52 -0300

    chore: change local json authentication file

--- provider.tf
+++ provider.tf
@@ -10,5 +10,5 @@ terraform {
 provider "google" {
   project     = "terraform-lab-442618"
   region      = var.region
-  credentials = "C:\\Users\\fabio\\workspace\\terraform-lab-442618-17fcd189cf4e.json"
+  credentials = "/home/fabmart/terraform-lab-442618-17fcd189cf4e.json"
 }
